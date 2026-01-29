8904f016319e6a1dadfa4d1d963a64615ba25706
Asmahan EL MHASSANI <83731610+Asmahan355@users.noreply.github.com>
2026-01-29 16:24:36 +0000

    les routes de product et customers

--- catalog/src/app/app-routing-module.ts
+++ catalog/src/app/app-routing-module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Products } from './products/products';
+import { Customers } from './customers/customers';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+    {path:"products", component:Products},
+  {path:"customers", component:Customers}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
